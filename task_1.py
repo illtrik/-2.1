6e4e8b62287eaccc9f83fe8b344f2efd49a8e79d
illtrik <bobuharina73@gmail.com>
2026-02-10 14:23:23 +0700

    Delete task_1.py

--- task_1.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def calculate(x, y):
-    result = 0
-    for i in range(0, 10):
-        if i % 2 == 0:
-            result += math.pow(x, i) / math.factorial(i)
-    return result + y
